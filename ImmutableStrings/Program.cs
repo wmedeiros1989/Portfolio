0664010732a940f1cf0d33621addc74a4a1b859c
wmedeiros1989 <159331893+wmedeiros1989@users.noreply.github.com>
2024-02-07 11:27:40 -0800

    Add files via upload

---
+++ ImmutableStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace ImmutableStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder first = new StringBuilder("This is a string");
+            StringBuilder second = new StringBuilder("This is a string");
+
+            Console.WriteLine($"first:  {first}");
+
+            Console.WriteLine("Clearing first...");
+            //first.Length = 0;
+            //Console.WriteLine($"first:  *{first}*");
+
+            //first.Append("Another string");
+            first.Clear();
+            first.Append("Another string");
+            first.Clear().Append("Another string").Append(" ").Append(second);
+            Console.WriteLine($"First: *{first}*");
+        }
+    }
+}
