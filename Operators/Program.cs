a77d002b5cda4480adc1fb57fb67864c7a7d241d
wmedeiros1989 <159331893+wmedeiros1989@users.noreply.github.com>
2024-02-07 11:28:33 -0800

    Add files via upload

---
+++ Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            x *= 34;
+            Console.WriteLine(x);
+
+            int y = 12;
+            y = y * 34;
+            Console.WriteLine(y);
+
+            int z = y -= 8;
+            Console.WriteLine($"y is {y}, z is {z}");
+        }
+    }
+}
